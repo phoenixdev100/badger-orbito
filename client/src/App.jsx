0a53f9d00c49036184fb0718ffacf5b8f5953f7a
Ayush Singh <2023505161.ayush@ug.sharda.ac.in>
2025-09-01 20:01:30 +0530

    frontend started

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
