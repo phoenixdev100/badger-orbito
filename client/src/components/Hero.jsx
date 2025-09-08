51e40a2bcfca68c8767003e789ef42d2d1fe21c2
Ayush Singh <2023505161.ayush@ug.sharda.ac.in>
2025-09-08 11:53:21 +0530

    tagline

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -38,14 +38,13 @@ const Hero = () => {
     >
       <div className="relative z-10 flex flex-col items-center">
         <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
-          Beta Now Live!
+          New and Shiny 💫
         </span>
         <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
-          Decrease your SaaS churn by over 90%
+          All Your Achivements In One Place
         </h1>
         <p className="my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
-          distinctio eum impedit nihil ipsum modi.
+          From coding platforms to learning hubs- Badger unifies your badges, so you can showcase your skills without chaos
         </p>
         <motion.button
           style={{
