fe0131032ad733a0a98ed64284e6bc9007a04e59
Deepak <deepakkumar312005@gmail.com>
2025-11-16 22:15:35 +0530

    Added something

--- server/server.js
+++ server/server.js
@@ -1,6 +1,42 @@
-// Polyfill for File object needed by undici
+// Polyfill for File and related objects needed by undici
 if (typeof global.File === 'undefined') {
-    global.File = class File { };
+    class Blob {
+        constructor(blobParts = [], options = {}) {
+            this._buffer = Buffer.concat(blobParts.map(part => Buffer.from(part)));
+            this.type = options.type || '';
+            this.size = this._buffer.length;
+        }
+        async arrayBuffer() {
+            return this._buffer.buffer.slice(
+                this._buffer.byteOffset,
+                this._buffer.byteOffset + this._buffer.byteLength
+            );
+        }
+        slice(start = 0, end = this.size, type = '') {
+            const slicedBuffer = this._buffer.slice(start, end);
+            return new Blob([slicedBuffer], { type });
+        }
+        stream() {
+            const { Readable } = require('stream');
+            return Readable.from(this._buffer);
+        }
+        text() {
+            return Promise.resolve(this._buffer.toString());
+        }
+    }
+
+    global.Blob = Blob;
+
+    class File extends Blob {
+        constructor(fileBits, fileName, options = {}) {
+            super(fileBits, options);
+            this.name = fileName;
+            this.lastModified = options.lastModified || Date.now();
+            this.webkitRelativePath = '';
+        }
+    }
+
+    global.File = File;
 }
 
 import express from 'express'
